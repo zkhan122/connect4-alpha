e00820391ce0b8d748d9f419e95bbcce1009f12c
Zayaan Khan <87833336+zkhan122@users.noreply.github.com>
2022-01-24 07:23:58 +0000

    Add files via upload

---
+++ player.py
@@ -0,0 +1,16 @@
+import math
+import numpy as np
+
+class Player:
+    def __init__(self, turn):
+        self.turn = turn
+    
+    def get_move(self):
+        pass
+
+class Player1(Player): #inherit
+    def __init__(self, turn):
+        super().__init__(turn)
+    
+    def get_move(self):
+        pass
