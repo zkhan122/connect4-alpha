19b0511b054267c17bb14bca3417ff9861803c5a
Zayaan Khan <87833336+zkhan122@users.noreply.github.com>
2022-02-10 11:46:08 +0000

    Update player.py

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+from base import *
 
 class Player:
     def __init__(self, turn):
@@ -8,11 +9,19 @@ class Player:
     def get_move(self):
         pass
 
-class Player1(Player): #inherit
+class RandomPlayer(Player): # inherit    
     def __init__(self, turn):
         super().__init__(turn)
     
-    def get_move(self):
-        pass
-    
-
+    def get_move(self, game, col):
+        valid_move = False
+        val = None
+        while not valid_move:
+            move = input(self.turn + '\'s turn. Input move from (0-6: ')
+            try:
+                val = int(move)
+                if val is not is_valid_location(self, col):
+                    raise ValueError
+            except ValueError:
+                print("Invalid move. Please try again")
+        return val
